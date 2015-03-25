f77be4124c3c53e23da82c75e81d5febc112845c
Guilherme Berghauser <tortoyoyo@gmail.com>
2015-03-25 13:11:55 -0300

    commit rapido para poder ajustar no trampo, criado branchs agora

--- public/js/controllers/CadFundoController.js
+++ public/js/controllers/CadFundoController.js
@@ -1,7 +1,13 @@
 angular.module('jogo').controller('CadFundoController',
     function($scope, $resource, $routeParams, initPage) {
-    	
-    	$scope.imgFundo = {img: 'img/cozinha.jpg', name: 'cozinha'};
+
+        // Contrutor ------
+        // ----------------
+
+        var contadorPaginacao = 1;
+        var contadorMax = 0;
+
+        // ----------------
 
         var init = {
             titulo: 'Escolha seu fundo',
@@ -9,5 +15,80 @@ angular.module('jogo').controller('CadFundoController',
         }
 
         initPage.pageCompleta($scope, init);
+
+
+        $scope.imagens = [{
+            name: "Sala",
+            img: "../img/sala.jpg"
+        }, {
+            name: "Cozinha",
+            img: "../img/cozinha.jpg"
+        }, {
+            name: "Quadro Negro",
+            img: "../img/quadro.jpg"
+        }, {
+            name: "Sala",
+            img: "../img/sala.jpg"
+        }, {
+            name: "Cozinha",
+            img: "../img/cozinha.jpg"
+        }, {
+            name: "Quadro Negro",
+            img: "../img/quadro.jpg"
+        }, {
+            name: "Sala",
+            img: "../img/sala.jpg"
+        }, {
+            name: "Cozinha",
+            img: "../img/cozinha.jpg"
+        }, {
+            name: "Quadro Negro",
+            img: "../img/quadro.jpg"
+        }];
+
+        $scope.imagensExibicao = [];
+
+        $scope.imgFundo = $scope.imagens[0];
+
+        $scope.mudarImagem = function(image) {
+            $scope.imgFundo = image;
+        }
+
+        $scope.avancarPaginacao = function() {
+            if (contadorPaginacao != contadorMax) {
+                contadorPaginacao++;
+                carregarProximos();
+
+            }
+        }
+
+        $scope.retrocederPaginacao = function() {
+            if (contadorPaginacao > 1) {
+                contadorPaginacao--;
+                carregarProximos();
+            }
+        }
+
+        var carregarProximos = function() {
+
+            contadorMax = $scope.imagens.length / 4;
+
+            if($scope.imagens.length % 4 != 0){
+                contatorMax = parseInt(contadorMax);
+                contadorMax = parseInt(contadorMax++);
+            }
+
+            var i = (contadorPaginacao * 4) - 4;
+            var max = i + 4;
+
+            $scope.imagensExibicao = [];
+
+            for (i; i < max; i++) {
+                $scope.imagensExibicao.push($scope.imagens[i]);
+            };
+        };
+
+
+        carregarProximos();
 
     });
