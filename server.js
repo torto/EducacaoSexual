f0c63d7d58abff9203f33321027b9cbac80c11e2
Guilherme Berghauser <tortoyoyo@gmail.com>
2015-10-07 11:08:56 -0300

    Servidor ajustado para iniciar o grunt quando iniciar o servidor

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var http = require('http');
 var app = require('./config/express')();
+var spawn = require('child_process').spawn;
+var cp = spawn(process.env.comspec, ['/c', 'grunt']); // ['/c', 'command', '-arg1', '-arg2']
 
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta '+ app.get('port'));
+cp.stdout.on("data", function(data) {
+  console.log(data.toString());
+});
+
+cp.stderr.on("data", function(data) {
+  console.error(data.toString());
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express Server escutando na porta ' + app.get('port'));
 })
