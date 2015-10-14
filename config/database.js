bbb9300b79965a2eec0a842ab218dce1824cbd65
tortoyoyo <tortoyoyo@gmail.com>
2015-10-14 01:06:33 -0300

    ajustando conexao banco

--- config/database.js
+++ config/database.js
@@ -1,24 +1,32 @@
+var config = require('./config/config')();
 var mongoose = require('mongoose');
 module.exports = function(uri) {
+  if (config.mongoUser) {
+    mongoose.connect(uri, {
+      user: config.mongoUser,
+      pass: config.mongoPass
+    });
+  } else {
     mongoose.connect(uri);
+  }
 
-    mongoose.connection.on('connected', function() {
-      console.log('Mongoose! Conectado em ' + uri);
+  mongoose.connection.on('connected', function() {
+    console.log('Mongoose! Conectado em ' + uri);
+  });
+  mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose! Desconectado de ' + uri);
+  });
+
+  mongoose.connection.on('error', function(erro) {
+    console.log('Mongoose! Erro na conexão: ' + erro);
+  });
+
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+      console.log('Mongoose! Desconectado pelo término da aplicação ');
+      // 0 indica que a finalização ocorreu sem erros
+      process.exit(0);
     });
-    mongoose.connection.on('disconnected', function() {
-      console.log('Mongoose! Desconectado de ' + uri);
-    });
+  });
 
-    mongoose.connection.on('error', function(erro) {
-      console.log('Mongoose! Erro na conexão: ' + erro);
-    });
-
-    process.on('SIGINT', function() {
-        mongoose.connection.close(function() {
-            console.log('Mongoose! Desconectado pelo término da aplicação ');
-              // 0 indica que a finalização ocorreu sem erros
-              process.exit(0);
-            });
-        });
-
-    };
+};
--- config/env/production.js
+++ config/env/production.js
@@ -15,5 +15,7 @@ module.exports = {
   clientSecret: process.env.CLIENT_SECRET,
   port: process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080,
   address: process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost',
-  domain: process.env.OPENSHIFT_APP_DNS
+  domain: process.env.OPENSHIFT_APP_DNS,
+  mongoUser: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+  mongoPass: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
 };
