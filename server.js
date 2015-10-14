5996b2b1896c1b0b812fa3a9a6a2613ccdddd0ed
tortoyoyo <tortoyoyo@gmail.com>
2015-10-13 23:53:31 -0300

    anta ajustou so o console e nao onde deveria

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var app = require('./config/express')();
 require('./config/passport.js')();
 require('./config/database.js')(config.db);
 console.log('port: '+config.port+' - ip: '+config.address);
-http.createServer(app).listen(config.port,config.andress, function() {
+http.createServer(app).listen(config.port,config.address, function() {
   console.log('Express Https Server '+ config.address+ ' (' + config.env+ ') escutando na porta ' + config.port);
 });
