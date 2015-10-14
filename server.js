ff3632c163f7751db39f3ebdccfaf6256fbac5bd
tortoyoyo <tortoyoyo@gmail.com>
2015-10-14 00:47:03 -0300

    aa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var app = require('./config/express')();
 require('./config/passport.js')();
 require('./config/database.js')(config.db);
-console.log('port: '+config.port+' - ip: '+config.address);
+console.log('port: '+config.port+' - ip: '+config.address); 
 http.createServer(app).listen(config.port,config.address, function() {
   console.log('Express Https Server '+ config.address+ ' (' + config.env+ ') escutando na porta ' + config.port);
 });
